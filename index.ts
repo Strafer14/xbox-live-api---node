55778172dee4398a9512421acbd89cce08b4a54a
Michael <strafer@strafer.dev>
2022-11-19 00:28:05 +0200

    cleaning up index file

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 import * as dotenv from 'dotenv';
-import { getDetailsForClip } from './lib/api';
 dotenv.config();
 
-const clipId = '271d86e5-8aec-430f-850b-70a49807d9b119';
-getDetailsForClip('Ninja', clipId).then(console.log).catch(console.error);
+export * from './lib/api';
